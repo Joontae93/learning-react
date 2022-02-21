f7e43551c192e666b79cd0790e39370f10459f4f
Joontae93 <kj.roelke@gmail.com>
2022-02-21 15:07:04 -0600

    init app

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,11 +1,89 @@
 "use strict";
 
 // JSX
-var userName = 'KJ';
-var age = 28;
-var location = 'Dallas, TX';
-var template = /*#__PURE__*/React.createElement("header", null, /*#__PURE__*/React.createElement("h1", null, userName), /*#__PURE__*/React.createElement("div", {
-  "class": "meta"
-}, /*#__PURE__*/React.createElement("span", null, "Age: ", age), /*#__PURE__*/React.createElement("span", null, "Location: ", location)));
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
+}; // This is a "Synthetic Event"
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderApp();
+  }
+};
+
+var removeOptions = function removeOptions() {
+  app.options = [];
+  renderApp();
+};
+/** Maps App.Options array to a new array with JSX
+ * @param {array} options the App Options
+ * @returns {array} Array of JSX strings
+ */
+
+
+function renderOptions(options) {
+  var output = options.map(function (option, i) {
+    {
+      return /*#__PURE__*/React.createElement("li", {
+        key: i
+      }, "Option ", i + 1, ": ", option);
+    }
+  });
+  return output;
+}
+
+function onMakeDecision() {
+  var choice = Math.floor(Math.random() * app.options.length);
+  var option = app.options[choice];
+  alert(option);
+}
+
+function renderApp() {
+  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("header", null, /*#__PURE__*/React.createElement("h1", null, app.title), /*#__PURE__*/React.createElement("span", null, app.subtitle)), /*#__PURE__*/React.createElement("section", null, /*#__PURE__*/React.createElement("p", null, app.options.length > 0 ? 'Here are your options:' : 'No Options'), /*#__PURE__*/React.createElement("ul", null, renderOptions(app.options)), /*#__PURE__*/React.createElement("form", {
+    onSubmit: onFormSubmit
+  }, /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    name: "option"
+  }), /*#__PURE__*/React.createElement("button", null, "Add Option")), /*#__PURE__*/React.createElement("button", {
+    disabled: app.options.length === 0,
+    onClick: onMakeDecision
+  }, "What Should I Do?"), /*#__PURE__*/React.createElement("button", {
+    disabled: app.options.length === 0,
+    onClick: removeOptions
+  }, "Remove All")));
+  ReactDOM.render(template, appRoot);
+}
+
+renderApp(); // let count = 0;
+// const addOne = () => {
+// 	count++;
+// 	renderCounterApp();
+// };
+// const subtractOne = () => {
+// 	count--;
+// 	renderCounterApp();
+// };
+// function reset() {
+// 	count = 0;
+// 	renderCounterApp();
+// }
+// function renderCounterApp() {
+// 	const templateTwo = (
+// 		<div>
+// 			<h1>Count: {count}</h1>
+// 			<button onClick={addOne}>+1</button>
+// 			<button onClick={subtractOne}>-1</button>
+// 			<button onClick={reset}>Reset</button>
+// 		</div>
+// 	);
+// 	ReactDOM.render(templateTwo, appRoot);
+// }
+// // renderCounterApp();
--- indecision-app/src/App.js
+++ indecision-app/src/App.js
@@ -1,17 +1,100 @@
 // JSX
-var userName = 'KJ';
-var age = 28;
-var location = 'Dallas, TX';
+const appRoot = document.getElementById('app');
 
-var template = (
-	<header>
-		<h1>{userName}</h1>
-		<div class="meta">
-			<span>Age: {age}</span>
-			<span>Location: {location}</span>
+const app = {
+	title: 'Indecision App',
+	subtitle: 'Put your life in the hands of a computer',
+	options: [],
+};
+
+// This is a "Synthetic Event"
+const onFormSubmit = (e) => {
+	e.preventDefault();
+	const option = e.target.elements.option.value;
+	if (option) {
+		app.options.push(option);
+		e.target.elements.option.value = '';
+		renderApp();
+	}
+};
+const removeOptions = () => {
+	app.options = [];
+	renderApp();
+};
+
+/** Maps App.Options array to a new array with JSX
+ * @param {array} options the App Options
+ * @returns {array} Array of JSX strings
+ */
+function renderOptions(options) {
+	const output = options.map((option, i) => {
+		{
+			return (
+				<li key={i}>
+					Option {i + 1}: {option}
+				</li>
+			);
+		}
+	});
+	return output;
+}
+function onMakeDecision() {
+	const choice = Math.floor(Math.random() * app.options.length);
+	const option = app.options[choice];
+	alert(option);
+}
+function renderApp() {
+	const template = (
+		<div>
+			<header>
+				<h1>{app.title}</h1>
+				<span>{app.subtitle}</span>
+			</header>
+			<section>
+				<p>
+					{app.options.length > 0 ? 'Here are your options:' : 'No Options'}
+				</p>
+				<ul>{renderOptions(app.options)}</ul>
+				<form onSubmit={onFormSubmit}>
+					<input type="text" name="option" />
+					<button>Add Option</button>
+				</form>
+				<button disabled={app.options.length === 0} onClick={onMakeDecision}>
+					What Should I Do?
+				</button>
+				<button disabled={app.options.length === 0} onClick={removeOptions}>
+					Remove All
+				</button>
+			</section>
 		</div>
-	</header>
-);
-var appRoot = document.getElementById('app');
+	);
+	ReactDOM.render(template, appRoot);
+}
+renderApp();
 
-ReactDOM.render(template, appRoot);
+// let count = 0;
+// const addOne = () => {
+// 	count++;
+// 	renderCounterApp();
+// };
+// const subtractOne = () => {
+// 	count--;
+// 	renderCounterApp();
+// };
+// function reset() {
+// 	count = 0;
+// 	renderCounterApp();
+// }
+
+// function renderCounterApp() {
+// 	const templateTwo = (
+// 		<div>
+// 			<h1>Count: {count}</h1>
+// 			<button onClick={addOne}>+1</button>
+// 			<button onClick={subtractOne}>-1</button>
+// 			<button onClick={reset}>Reset</button>
+// 		</div>
+// 	);
+// 	ReactDOM.render(templateTwo, appRoot);
+// }
+// // renderCounterApp();
