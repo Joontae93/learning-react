040d6592e5e19b3a24b6125a891df7e8b286c3f0
Joontae93 <kj.roelke@gmail.com>
2022-02-23 11:47:19 -0600

    UX fix

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-	<Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
+	<Modal
+		isOpen={!!props.selectedOption}
+		contentLabel="Selected Option"
+		onRequestClose={props.clearSelectedOption}
+	>
 		<h3>Selected Option</h3>
 		{props.selectedOption && <p>{props.selectedOption}</p>}
 		<button onClick={props.clearSelectedOption}>Ugh fine, I'll do it.</button>
