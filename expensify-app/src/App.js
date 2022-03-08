ed35f6e067f0346917a3072b6e5cfe4cc9c90ddd
Joontae93 <kj.roelke@gmail.com>
2022-03-08 16:41:23 -0600

    init expensify app

---
+++ expensify-app/src/App.js
@@ -0,0 +1,6 @@
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>A Boilerplate.</p>, document.getElementById('app'));
---
+++ expensify-app/webpack.config.js
@@ -0,0 +1,30 @@
+// entry point
+// outpout
+const path = require('path');
+
+module.exports = {
+	entry: './src/App.js',
+	output: { path: path.join(__dirname, 'public'), filename: 'bundle.js' },
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.s?css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	devtool: 'eval-cheap-module-source-map',
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		port: 8080,
+		hot: 'only',
+	},
+};
